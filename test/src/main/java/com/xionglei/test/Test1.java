28d2144d9fc51ce34e142ed102f75c88b359393b
xionglei <302086963@qq.com>
2016-08-24 23:06:06 +0800

    修改system

--- test/src/main/java/com/xionglei/test/Test1.java
+++ test/src/main/java/com/xionglei/test/Test1.java
@@ -13,7 +13,7 @@ public class Test1 {
 	private static final Logger logger = LoggerFactory.getLogger(Test1.class);
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		logger.info("hello");
+		logger.info("hellodfgdfgd");
 	}
 
 }
