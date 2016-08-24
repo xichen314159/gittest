a708a5a69ce5039710fcbe92c16a3b9e251737c7
xionglei <302086963@qq.com>
2016-08-24 23:01:37 +0800

    init

---
+++ test/src/main/java/com/xionglei/test/Test1.java
@@ -0,0 +1,19 @@
+package com.xionglei.test;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author	xionglei
+ * @date 	2016年8月23日 下午11:02:52
+ */
+public class Test1 {
+
+	private static final Logger logger = LoggerFactory.getLogger(Test1.class);
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		logger.info("hello");
+	}
+
+}
---
+++ test/src/main/java/com/xionglei/test/Test2.java
@@ -0,0 +1,14 @@
+package com.xionglei.test;
+/**
+ *
+ * @author	xionglei
+ * @date 	2016年8月23日 下午11:05:43
+ */
+public class Test2 {
+
+	public static void main(String[] args) {
+		for (int i = 0, len = args.length; i < len; i++) {
+			System.out.println("345345");
+		}
+	}
+}
